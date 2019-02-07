332f786e42a2a10a8980cce0ad8e3a18cba06967
terencec <chalkterence@gmail.com>
2019-02-07 16:38:52 +0800

    done with first part

--- server.js
+++ server.js
@@ -1,11 +1,16 @@
 var express = require('express');
 var app = express();
+var bodyParser =  require('body-parser');
 
-app.get('/about', function(req, res) {
-	res.send('FeelsWeirdMan');
-})
-
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(__dirname + "/public"));
+
+app.post('/team', function(req, res) {
+	let name = req.body;
+	console.log(JSON.stringify(name));
+	res.send("Received!");
+});
 
 app.listen(3000, function() {
 	console.log("started :)");
