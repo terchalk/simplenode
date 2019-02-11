002f49a07d4cd1dc97ebf318fdf88025fb9f3f8f
terencec <chalkterence@gmail.com>
2019-02-11 18:14:49 +0800

    done with implementation, improving interface later

--- index.js
+++ index.js
@@ -24,18 +24,35 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(__dirname + "/public"));
 
 app.post('/team', function(req, res) {
-  let name = req.body;
+  let name = req.body.name;
   console.log(JSON.stringify(name));
-  participantName = req.body;
 
   // Load client secrets from a local file.
   fs.readFile('credentials.json', (err, content) => {
     if (err) return console.log('Error loading client secret file:', err);
     // Authorize a client with credentials, then call the Google Apps Script API.
-    authorize(JSON.parse(content), callAppsScript);
+    // authorize(JSON.parse(content), callAppsScript);
+
+    const {client_secret, client_id, redirect_uris} = JSON.parse(content).web;
+    const oAuth2Client = new google.auth.OAuth2(
+        client_id, client_secret, redirect_uris[0]);
+
+    // Check if we have previously stored a token.
+    fs.readFile(TOKEN_PATH, (err, token) => {
+      if (err) {
+        return getAccessToken(oAuth2Client);
+      } else {
+        oAuth2Client.setCredentials(JSON.parse(token));
+        callAppsScript2(oAuth2Client, name).then(() =>{
+          res.redirect('https://docs.google.com/forms/d/e/1FAIpQLSdOSbvwTkpsezfCJQph8tJLDCjOsaKYvbwCkQwM43vdwqiedg/viewform?usp=sf_link');
+        }).catch(() => {
+          console.log('google scripts error');
+        }); 
+      }
+    });
   });
 
-  res.redirect('https://docs.google.com/forms/d/e/1FAIpQLSdOSbvwTkpsezfCJQph8tJLDCjOsaKYvbwCkQwM43vdwqiedg/viewform?usp=sf_link');
+  // res.redirect('https://docs.google.com/forms/d/e/1FAIpQLSdOSbvwTkpsezfCJQph8tJLDCjOsaKYvbwCkQwM43vdwqiedg/viewform?usp=sf_link');
 });
 
 app.listen(3000, function() {
@@ -70,7 +87,7 @@ function authorize(credentials, callback) {
  * @param {google.auth.OAuth2} oAuth2Client The OAuth2 client to get token for.
  * @param {getEventsCallback} callback The callback for the authorized client.
  */
-function getAccessToken(oAuth2Client, callback) {
+function getAccessToken(oAuth2Client) {
   const authUrl = oAuth2Client.generateAuthUrl({
     access_type: 'offline',
     scope: SCOPES,
@@ -90,7 +107,6 @@ function getAccessToken(oAuth2Client, callback) {
         if (err) console.error(err);
         console.log('Token stored to', TOKEN_PATH);
       });
-      callback(oAuth2Client);
     });
   });
 }
@@ -118,5 +134,33 @@ function callAppsScript(auth) {
       return;
     }
   });
+}
+
+function callAppsScript2(auth, name) {
+  const scriptId = '11jQnbvTIG_ov5Y-E4M4T2yzKvOMIvVT_Gu24ba1AMYW0Z24LTM767HRn';
+  const script = google.script('v1');
+  
+  return new Promise((resolve, reject) => {
+   script.scripts.run({
+      auth: auth,
+      resource: {
+        function: 'changeFormTitle',
+        parameters: [
+          name
+        ],
+      },
+      scriptId: scriptId,
+    }, function(err, resp) {
+      // console.log(JSON.stringify(resp.data.error.details));
+      console.log(resp);
+      if (err) {
+        // The API encountered a problem before the script started executing.
+        console.log('The API returned an error: ' + err);
+        reject();
+      }
+
+      resolve();
+    });   
+  })
 
 }
