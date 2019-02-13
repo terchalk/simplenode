04cb36a7137df4d2a75218c7b90998893bb19a22
terencec <chalkterence@gmail.com>
2019-02-13 15:36:16 +0800

    roughly completed

--- index.js
+++ index.js
@@ -14,6 +14,7 @@ const SCOPES =
   "https://www.googleapis.com/auth/drive",
   "https://www.googleapis.com/auth/forms"
 ];
+
 // The file token.json stores the user's access and refresh tokens, and is
 // created automatically when the authorization flow completes for the first
 // time.
@@ -25,13 +26,10 @@ app.use(express.static(__dirname + "/public"));
 
 app.post('/team', function(req, res) {
   let name = req.body.name;
-  console.log(JSON.stringify(name));
 
   // Load client secrets from a local file.
   fs.readFile('credentials.json', (err, content) => {
     if (err) return console.log('Error loading client secret file:', err);
-    // Authorize a client with credentials, then call the Google Apps Script API.
-    // authorize(JSON.parse(content), callAppsScript);
 
     const {client_secret, client_id, redirect_uris} = JSON.parse(content).web;
     const oAuth2Client = new google.auth.OAuth2(
@@ -43,7 +41,7 @@ app.post('/team', function(req, res) {
         return getAccessToken(oAuth2Client);
       } else {
         oAuth2Client.setCredentials(JSON.parse(token));
-        callAppsScript2(oAuth2Client, name).then(() =>{
+        callAppScript(oAuth2Client, name).then(() => {
           res.redirect('https://docs.google.com/forms/d/e/1FAIpQLSdOSbvwTkpsezfCJQph8tJLDCjOsaKYvbwCkQwM43vdwqiedg/viewform?usp=sf_link');
         }).catch(() => {
           console.log('google scripts error');
@@ -51,35 +49,11 @@ app.post('/team', function(req, res) {
       }
     });
   });
-
-  // res.redirect('https://docs.google.com/forms/d/e/1FAIpQLSdOSbvwTkpsezfCJQph8tJLDCjOsaKYvbwCkQwM43vdwqiedg/viewform?usp=sf_link');
 });
 
 app.listen(3000, function() {
-  console.log("started :)");
+  console.log("started!");
 });
-
-/**
- * Create an OAuth2 client with the given credentials, and then execute the
- * given callback function.
- * @param {Object} credentials The authorization client credentials.
- * @param {function} callback The callback to call with the authorized client.
- */
-function authorize(credentials, callback) {
-  const {client_secret, client_id, redirect_uris} = credentials.web;
-  const oAuth2Client = new google.auth.OAuth2(
-      client_id, client_secret, redirect_uris[0]);
-
-  // Check if we have previously stored a token.
-  fs.readFile(TOKEN_PATH, (err, token) => {
-    if (err) {
-      return getAccessToken(oAuth2Client, callback);
-    } else {
-      oAuth2Client.setCredentials(JSON.parse(token));
-      callback(oAuth2Client); 
-    }
-  });
-}
 
 /**
  * Get and store new token after prompting for user authorization, and then
@@ -111,32 +85,7 @@ function getAccessToken(oAuth2Client) {
   });
 }
 
-/**
- * Creates a new script project, upload a file, and log the script's URL.
- * @param {google.auth.OAuth2} auth An authorized OAuth2 client.
- */
-function callAppsScript(auth) {
-  const scriptId = '11jQnbvTIG_ov5Y-E4M4T2yzKvOMIvVT_Gu24ba1AMYW0Z24LTM767HRn';
-  const script = google.script('v1');
-  
-  script.scripts.run({
-    auth: auth,
-    resource: {
-      function: 'change'
-    },
-    scriptId: scriptId,
-  }, function(err, resp) {
-    // console.log(JSON.stringify(resp.data.error.details));
-    // console.log(resp);
-    if (err) {
-      // The API encountered a problem before the script started executing.
-      console.log('The API returned an error: ' + err);
-      return;
-    }
-  });
-}
-
-function callAppsScript2(auth, name) {
+function callAppScript(auth, name) {
   const scriptId = '11jQnbvTIG_ov5Y-E4M4T2yzKvOMIvVT_Gu24ba1AMYW0Z24LTM767HRn';
   const script = google.script('v1');
   
@@ -151,8 +100,6 @@ function callAppsScript2(auth, name) {
       },
       scriptId: scriptId,
     }, function(err, resp) {
-      // console.log(JSON.stringify(resp.data.error.details));
-      console.log(resp);
       if (err) {
         // The API encountered a problem before the script started executing.
         console.log('The API returned an error: ' + err);
@@ -162,5 +109,4 @@ function callAppsScript2(auth, name) {
       resolve();
     });   
   })
-
 }
