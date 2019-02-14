2e6391f2f29e6f408b7cb622b1f82da021458167
System Administrator <root@terftw.local>
2019-02-14 09:57:02 +0800

    fix missing parenthesis bug

--- index.js
+++ index.js
@@ -16,6 +16,7 @@ app.post('/team', function(req, res) {
   let name = req.body.name;
 
   res.redirect('https://docs.google.com/forms/d/e/1FAIpQLSdOSbvwTkpsezfCJQph8tJLDCjOsaKYvbwCkQwM43vdwqiedg/viewform?usp=pp_url&entry.1658505065=' + name);
+});
 
 app.listen(3000, function() {
   console.log("started!");
